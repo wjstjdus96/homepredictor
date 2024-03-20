af54dda2c60c3539a1fc031af0a006ee57b8f2e2
kinggh <fmaxgo@naver.com>
2024-03-20 15:29:58 +0900

    Feat: Searchbar 필터링 구현 Searchbar 필터링 로직 구현 검색된 주소가 있을 때와 없을 때 나타나는 컴포넌트 구성 검색 결과창 가시성 유무 구현

--- src/pages/Home/Components/SearchBar.tsx
+++ src/pages/Home/Components/SearchBar.tsx
@@ -2,7 +2,10 @@ import styled from "styled-components"
 import { useState, useEffect } from "react"
 import axios from "axios"
 
+import { Town, townData } from "../DummyData/DummyData"
+
 const SearchBarContainer = styled.div`
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -12,6 +15,9 @@ const SearchBarContainer = styled.div`
         height: 60px;
     }
     margin-top: 7.5%;
+    @media (max-width: 600px){
+        margin-top: 20%;
+    }
     padding: 5px;
     border: 6px solid white;
     border-radius: 100px;
@@ -63,15 +69,116 @@ const SearchImg = styled.img`
         height: 20px;
     }
 `
+const SearchResultDiv = styled.div`
+    position: absolute;
+    top: 105px;
+    @media (max-width: 600px){
+        top: 85px;
+    }
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: calc(100% - 60px);
+    max-height: 500px;
+    padding: 10px 30px;
+    background-color: white;
+    border-radius: 50px;
+`
+const ScrollDiv = styled.div`
+    width: 100%;
+    overflow: scroll;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: rgba(0,0,0,0)
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: #c2c2c2;
+        border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+        background: #969696;
+    }
+`
+const SearchResultContent = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+    padding: 5px 0;
+    color: rgba(0,0,0,0.75);
+    font-size: 18px;
+    @media (max-width: 600px){
+        font-size: 14px;
+    }
+    &:hover{
+        font-weight: bold;
+    }
+    transition: color 0.25s ease;
+    overflow:hidden;
+`
+const NoResearchContent = styled(SearchResultContent)`
+    &:hover{
+        font-weight: 400;
+    }
+`
+const HighLightSpan = styled.span`
+    font-weight: bold;
+    color: #378CE7;
+`
 export const SearchBar = () => {
+    const [address, setAddress] = useState<string>('')
+    const [selectedTownData, setSelectedTownData] = useState<Town[]>([])
+    const typeAddress = (e: any) => {
+        setAddress(e.target.value)
+        setSelectedTownData(townData.filter(el => el.town.includes(e.target.value)))
+    }
+    const boldMatchingSubstring = (str: string, substr: string) => {
+        const index = str.indexOf(substr);
+        if (index !== -1) {
+            return (
+                <span>
+                    {str.substring(0, index)}
+                    <HighLightSpan>{substr}</HighLightSpan>
+                    {str.substring(index + substr.length)}
+                </span>
+            );
+        }
+        return str;
+    };
+
     return (
         <SearchBarContainer>
             <SearchBarMainDiv>
-                <SearchContent type='text' placeholder="주소, 건물명 등을 입력하세요"></SearchContent>
+                <SearchContent
+                    type='text' placeholder="주소, 건물명 등을 입력하세요"
+                    onChange={typeAddress}
+                ></SearchContent>
                 <SearchButton>
                     <SearchImg src={`${process.env.PUBLIC_URL}/img/search.png`}></SearchImg>
                 </SearchButton>
             </SearchBarMainDiv>
+            {address.length > 0 ? (
+                <SearchResultDiv>
+                    {selectedTownData.length > 0 ? (
+                        <ScrollDiv>
+                            {selectedTownData.map(el => (
+                                <SearchResultContent>
+                                    {boldMatchingSubstring(el.town, address)}
+                                </SearchResultContent>
+                            ))}
+                        </ScrollDiv>
+                        ) : (
+                            <NoResearchContent>검색 결과가 없습니다</NoResearchContent>
+                        )}
+                </SearchResultDiv>
+                ) : ''}
         </SearchBarContainer>
     )
 }
