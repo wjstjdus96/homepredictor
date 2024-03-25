6c5cf86d39a92e0a4c55a5dad978d82cdd2ebea1
seoyeon <wjstjdus96@naver.com>
2024-03-25 19:13:29 +0900

    feat: api 연결 및 헤더 라우팅 기능

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,33 @@
 import styled from "styled-components";
 import { useState, useEffect } from "react";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 
-interface CurrentPage{
+interface CurrentPage {
   curpage: string;
 }
 
 export default function Header() {
-  const location = useLocation().pathname
-  const [curPage, setCurPage] = useState('/')
+  const location = useLocation().pathname;
+  const navigate = useNavigate();
+  const [curPage, setCurPage] = useState("/");
+  const onClickLogo = () => {
+    navigate("/");
+  };
 
   useEffect(() => {
-    setCurPage(location)
-  }, [location])
+    setCurPage(location);
+  }, [location]);
 
   return (
     <HeaderLayout curpage={curPage}>
-      <h2>Home Predictor</h2>
+      <h2 onClick={onClickLogo}>Home Predictor</h2>
     </HeaderLayout>
   );
 }
 
 const HeaderLayout = styled.header<CurrentPage>`
-  background-color: ${props => props.curpage === '/' ? 'rgba(0,0,0,0)' : props.theme.colors.primary};
+  background-color: ${(props) =>
+    props.curpage === "/" ? "rgba(0,0,0,0)" : props.theme.colors.primary};
   height: 7vh;
   display: flex;
   align-items: center;
@@ -31,5 +36,6 @@ const HeaderLayout = styled.header<CurrentPage>`
     color: white;
     margin-left: 1rem;
     font-weight: 700;
+    cursor: pointer;
   }
 `;
