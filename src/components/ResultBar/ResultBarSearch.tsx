f0e9e95841362e368368b4cbaee8a611cd7e81df
seoyeon <wjstjdus96@naver.com>
2024-03-26 19:44:25 +0900

    feat: 결과페이지 검색창 기능

--- src/components/ResultBar/ResultBarSearch.tsx
+++ src/components/ResultBar/ResultBarSearch.tsx
@@ -2,21 +2,70 @@ import React from "react";
 import styled from "styled-components";
 import { flexCenter } from "../../styles/GlobalStyles";
 import { FaSearch } from "react-icons/fa";
+import { HouseInfo, SearchBar } from "../../pages/Home/Components/SearchBar";
+import { useSearch } from "../../hooks/useSearch";
+import { useNavigate } from "react-router-dom";
+import { addressState } from "../../pages/Home/State/AddressState";
+import { useSetRecoilState } from "recoil";
 
 export default function ResultBarSearch() {
+  const { address, selectedTownData, typeAddress } = useSearch();
+  const setClickedAddress = useSetRecoilState(addressState);
+  const navigate = useNavigate();
+  const showBuildingInfo = (id: number, address: string) => {
+    setClickedAddress(address);
+    navigate(`/result/:${id}`);
+  };
+  const boldMatchingSubstring = (str: string, substr: string) => {
+    const index = str.indexOf(substr);
+    if (index !== -1) {
+      return (
+        <span>
+          {str.substring(0, index)}
+          <HighLightSpan>{substr}</HighLightSpan>
+          {str.substring(index + substr.length)}
+        </span>
+      );
+    }
+    return str;
+  };
   return (
     <ResultBarSearchContainer>
-      <FaSearch />
-      <input placeholder="주소, 건물명 등을 입력하세요" />
+      <ResultSearchMainDiv>
+        <FaSearch />
+        <input
+          placeholder="주소, 건물명 등을 입력하세요"
+          onChange={typeAddress}
+        />
+      </ResultSearchMainDiv>
+      {address.length > 0 && (
+        <ResultSearchResultDiv>
+          {selectedTownData !== null && selectedTownData.length > 0 ? (
+            <ScrollDiv>
+              {selectedTownData.map((el: HouseInfo, idx: number) => (
+                <SearchResultContent
+                  key={idx}
+                  onClick={() => showBuildingInfo(el.id, el.address)}
+                >
+                  {boldMatchingSubstring(el.address, address)}
+                </SearchResultContent>
+              ))}
+            </ScrollDiv>
+          ) : (
+            <NoResearchContent>검색 결과가 없습니다</NoResearchContent>
+          )}
+        </ResultSearchResultDiv>
+      )}
     </ResultBarSearchContainer>
   );
 }
 
 const ResultBarSearchContainer = styled.div`
-  ${flexCenter}
   padding: 15px ${(props) => props.theme.padding.searchBar} 0;
   position: relative;
+`;
 
+const ResultSearchMainDiv = styled.div`
   input {
     width: 100%;
     height: 30px;
@@ -35,6 +84,67 @@ const ResultBarSearchContainer = styled.div`
   svg {
     position: absolute;
     left: 1.3rem;
+    top: 1.3rem;
     color: ${(props) => props.theme.colors.primary};
   }
+`;
+
+const ResultSearchResultDiv = styled.div`
+  position: absolute;
+  width: 88%;
+  left: 1rem;
+  height: 20rem;
+  top: 3rem;
+  background-color: white;
+  z-index: 10;
+  border-radius: 30px;
+  padding: 3px 12px;
+  overflow: scroll;
+  overflow-x: hidden;
+  &::-webkit-scrollbar {
+    width: 3px;
+  }
+  &::-webkit-scrollbar-track {
+    background: rgba(0, 0, 0, 0);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #c2c2c2;
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: #969696;
+  }
+`;
+
+const ScrollDiv = styled.div`
+  height: 100%;
+  padding: 0 10px;
+`;
+
+const SearchResultContent = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  padding: 5px 0;
+  margin: 3px 0;
+  color: rgba(0, 0, 0, 0.75);
+  font-size: 13px;
+  &:hover {
+    font-weight: bold;
+  }
+  transition: color 0.25s ease;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+const NoResearchContent = styled(SearchResultContent)`
+  &:hover {
+    font-weight: 400;
+  }
+`;
+
+const HighLightSpan = styled.span`
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.primary};
 `;
--- src/pages/Home/Components/SearchBar.tsx
+++ src/pages/Home/Components/SearchBar.tsx
@@ -1,216 +1,210 @@
-import styled from "styled-components"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { addressState } from "../State/AddressState"
-import { useSetRecoilState } from "recoil"
-import axios from "axios"
+import styled from "styled-components";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { addressState } from "../State/AddressState";
+import { useSetRecoilState } from "recoil";
+import axios from "axios";
+import { useSearch } from "../../../hooks/useSearch";
 
-interface HouseInfo{
-    id:number;
-    address:string;
+export interface HouseInfo {
+  id: number;
+  address: string;
 }
 
 export const SearchBar = () => {
-    const [address, setAddress] = useState<string>('')
-    const [selectedTownData, setSelectedTownData] = useState<any>([])
-    const setClickedAddress = useSetRecoilState(addressState)
-    //일단 공공데이터로 Test
-    const typeAddress = (e: any) => {
-        const apiUrl = 'https://api.home-predictor.com/apartments'    
-        let timer
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-            axios.get(`${apiUrl}?address=${e.target.value}`, {
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-                }
-            })
-            .then(response => {
-                setSelectedTownData(response.data.slice(0, 20))
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error)
-            })
-        }, 500)
-        setAddress(e.target.value)
+  const { address, selectedTownData, typeAddress } = useSearch();
+  const setClickedAddress = useSetRecoilState(addressState);
+  const navigate = useNavigate();
+  const showBuildingInfo = (id: number, address: string) => {
+    setClickedAddress(address);
+    navigate(`/result/:${id}`);
+  };
+  const boldMatchingSubstring = (str: string, substr: string) => {
+    const index = str.indexOf(substr);
+    if (index !== -1) {
+      return (
+        <span>
+          {str.substring(0, index)}
+          <HighLightSpan>{substr}</HighLightSpan>
+          {str.substring(index + substr.length)}
+        </span>
+      );
     }
-    const navigate = useNavigate()
-    const showBuildingInfo = (id:number, address:string) => {
-        setClickedAddress(address)
-        navigate(`/result/:${id}`)
-    }
-    const boldMatchingSubstring = (str: string, substr: string) => {
-        const index = str.indexOf(substr)
-        if (index !== -1) {
-            return (
-                <span>
-                    {str.substring(0, index)}
-                    <HighLightSpan>{substr}</HighLightSpan>
-                    {str.substring(index + substr.length)}
-                </span>
-            )
-        }
-        return str
-    }
+    return str;
+  };
 
-    return (
-        <SearchBarContainer>
-            <SearchBarMainDiv>
-                <SearchContent
-                    type='text' placeholder="주소, 건물명 등을 입력하세요"
-                    onChange={typeAddress}
-                ></SearchContent>
-                <SearchButton>
-                    <img src={`${process.env.PUBLIC_URL}/img/search.png`} alt='Searchbar'></img>
-                </SearchButton>
-            </SearchBarMainDiv>
-            {address.length > 0 ? (
-                <SearchResultDiv>
-                    {selectedTownData !== null && selectedTownData.length > 0 ? (
-                        <ScrollDiv>
-                            {selectedTownData.map((el:HouseInfo, idx:number) => (
-                                <SearchResultContent key={idx} onClick={() => showBuildingInfo(el.id, el.address)}>
-                                    {boldMatchingSubstring(el.address, address)}
-                                </SearchResultContent>
-                            ))}
-                        </ScrollDiv>
-                        ) : (
-                            <NoResearchContent>검색 결과가 없습니다</NoResearchContent>
-                        )}
-                </SearchResultDiv>
-                ) : ''}
-        </SearchBarContainer>
-    )
-}
+  return (
+    <SearchBarContainer>
+      <SearchBarMainDiv>
+        <SearchContent
+          type="text"
+          placeholder="주소, 건물명 등을 입력하세요"
+          onChange={typeAddress}
+        ></SearchContent>
+        <SearchButton>
+          <img
+            src={`${process.env.PUBLIC_URL}/img/search.png`}
+            alt="Searchbar"
+          ></img>
+        </SearchButton>
+      </SearchBarMainDiv>
+      {address.length > 0 ? (
+        <SearchResultDiv>
+          {selectedTownData !== null && selectedTownData.length > 0 ? (
+            <ScrollDiv>
+              {selectedTownData.map((el: HouseInfo, idx: number) => (
+                <SearchResultContent
+                  key={idx}
+                  onClick={() => showBuildingInfo(el.id, el.address)}
+                >
+                  {boldMatchingSubstring(el.address, address)}
+                </SearchResultContent>
+              ))}
+            </ScrollDiv>
+          ) : (
+            <NoResearchContent>검색 결과가 없습니다</NoResearchContent>
+          )}
+        </SearchResultDiv>
+      ) : (
+        ""
+      )}
+    </SearchBarContainer>
+  );
+};
 
 const SearchBarContainer = styled.div`
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 80%;
-    height: 100px;
-    @media (max-width: 600px){
-        height: 80px;
-    }
-    margin-top: 7.5%;
-    @media (max-width: 600px){
-        margin-top: 20%;
-    }
-    padding: 5px;
-    border: 6px solid white;
-    border-radius: 100px;
-`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  height: 100px;
+  @media (max-width: 600px) {
+    height: 80px;
+  }
+  margin-top: 7.5%;
+  @media (max-width: 600px) {
+    margin-top: 20%;
+  }
+  padding: 5px;
+  border: 6px solid white;
+  border-radius: 100px;
+`;
 const SearchBarMainDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: calc(100% - 10px);
-    height: 70px;
-    @media (max-width: 600px){
-        height: 50px;
-    }
-    background: linear-gradient(to right, #ffffff 0%, #ffffff 88%, ${(props) => props.theme.colors.primary} 88%, ${(props) => props.theme.colors.primary} 100%);
-    border-radius: 100px;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: calc(100% - 10px);
+  height: 70px;
+  @media (max-width: 600px) {
+    height: 50px;
+  }
+  background: linear-gradient(
+    to right,
+    #ffffff 0%,
+    #ffffff 88%,
+    ${(props) => props.theme.colors.primary} 88%,
+    ${(props) => props.theme.colors.primary} 100%
+  );
+  border-radius: 100px;
+`;
 const SearchContent = styled.input`
-    display: flex;
-    align-items: center;
-    width: 88%;
-    height: 100%;
-    padding: 0 30px;
-    background-color: rgba(0,0,0,0);
-    color: rgba(0,0,0,0.5);
-    border: 0;
-    outline: none;
-    font-size: 16px;
-    @media (max-width: 600px){
-        font-size: 14px;
-    }
-`
+  display: flex;
+  align-items: center;
+  width: 88%;
+  height: 100%;
+  padding: 0 30px;
+  background-color: rgba(0, 0, 0, 0);
+  color: rgba(0, 0, 0, 0.5);
+  border: 0;
+  outline: none;
+  font-size: 16px;
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
+`;
 const SearchButton = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 12%;
-    height: 100%;
-    background-color:${(props) => props.theme.colors.primary};
-    border-radius: 0 100px 100px 0;
-    &:hover{
-        background-color: #2b77c9;
-    }
-    img{
-        width: 40px;
-        height: 40px;
-        @media (max-width: 600px){
-            width: 20px;
-            height: 20px;
-        }
-    }
-`
-const SearchResultDiv = styled.div`
-    position: absolute;
-    top: 105px;
-    @media (max-width: 600px){
-        top: 85px;
-    }
-    left: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 100%;
-    max-height: 60vh;
-    padding: 10px 30px;
-    background-color: white;
-    border-radius: 50px;
-`
-const ScrollDiv = styled.div`
-    width: 100%;
-    overflow: scroll;
-    overflow-x: hidden;
-    &::-webkit-scrollbar {
-        width: 8px;
-    }
-
-    &::-webkit-scrollbar-track {
-        background: rgba(0,0,0,0)
-    }
-
-    &::-webkit-scrollbar-thumb {
-        background: #c2c2c2;
-        border-radius: 10px;
-    }
-
-    &::-webkit-scrollbar-thumb:hover {
-        background: #969696;
-    }
-`
-const SearchResultContent = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 12%;
+  height: 100%;
+  background-color: ${(props) => props.theme.colors.primary};
+  border-radius: 0 100px 100px 0;
+  &:hover {
+    background-color: #2b77c9;
+  }
+  img {
+    width: 40px;
     height: 40px;
-    padding: 5px 0;
-    margin: 3px 0;
-    color: rgba(0,0,0,0.75);
-    font-size: 18px;
-    @media (max-width: 600px){
-        font-size: 14px;
+    @media (max-width: 600px) {
+      width: 20px;
+      height: 20px;
     }
-    &:hover{
-        font-weight: bold;
-    }
-    transition: color 0.25s ease;
-    overflow:hidden;
-    cursor:pointer;
-`
-const NoResearchContent = styled(SearchResultContent)`
-    &:hover{
-        font-weight: 400;
-    }
-`
-const HighLightSpan = styled.span`
+  }
+`;
+const SearchResultDiv = styled.div`
+  position: absolute;
+  top: 105px;
+  @media (max-width: 600px) {
+    top: 85px;
+  }
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  max-height: 60vh;
+  padding: 10px 30px;
+  background-color: white;
+  border-radius: 50px;
+`;
+const ScrollDiv = styled.div`
+  width: 100%;
+  overflow: scroll;
+  overflow-x: hidden;
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: rgba(0, 0, 0, 0);
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #c2c2c2;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #969696;
+  }
+`;
+const SearchResultContent = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  padding: 5px 0;
+  margin: 3px 0;
+  color: rgba(0, 0, 0, 0.75);
+  font-size: 18px;
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
+  &:hover {
     font-weight: bold;
-    color: ${(props) => props.theme.colors.primary};
-`
+  }
+  transition: color 0.25s ease;
+  overflow: hidden;
+  cursor: pointer;
+`;
+const NoResearchContent = styled(SearchResultContent)`
+  &:hover {
+    font-weight: 400;
+  }
+`;
+const HighLightSpan = styled.span`
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.primary};
+`;
